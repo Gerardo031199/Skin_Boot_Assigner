b303cb33a9f5eea78ef8a405df256f17f2479996
moth1995 <marcos-flores@live.com.ar>
2022-09-09 00:54:59 -0300

    fix for resource file

--- setup.bat
+++ setup.bat
@@ -3,7 +3,7 @@ set PY_FILE=main.py
 set PROJECT_NAME=Skin Boot Assigner
 set VERSION=1.0.0
 set FILE_VERSION=file_version_info.txt 
-set EXTRA_ARG=--add-data=resource.png;resource.png  --additional-hooks-dir=. 
+set EXTRA_ARG=--add-data=resource.png;.  --additional-hooks-dir=. 
 
 pyinstaller --onefile --noconsole "%PY_FILE%" --name "%PROJECT_NAME%_%VERSION%" %EXTRA_ARG% --version-file "%FILE_VERSION%"
 
